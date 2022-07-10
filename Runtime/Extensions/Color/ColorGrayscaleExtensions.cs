4beccc0aabfb1dd6ee8810caf34c577bee516deb
sponticelli <sandro.ponticelli@gmail.com>
2022-07-10 22:09:19 +0200

    fix: grayscale

--- Runtime/Extensions/Color/ColorGrayscaleExtensions.cs
+++ Runtime/Extensions/Color/ColorGrayscaleExtensions.cs
@@ -5,14 +5,6 @@ namespace LiteNinja.Colors.Extensions
     public static class ColorGrayscaleExtensions
     {
         /// <summary>
-        /// Sets the saturation of the color to zero.
-        /// </summary>
-        public static void Grayscale(this ref Color color)
-        {
-            color.Desaturate(1f);
-        }
-
-        /// <summary>
         /// Returns a new instance of the color with zero saturation.
         /// </summary>
         public static Color Grayscale(this Color color)
